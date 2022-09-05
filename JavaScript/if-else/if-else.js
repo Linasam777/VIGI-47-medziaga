ff563c6cd7853ab1dff51bef6762736ae62c0eaa
Jonas Girdzijauskas <jonas.girdzijauskas@gmail.com>
2022-09-05 18:07:04 +0200

    added if-else

---
+++ JavaScript/if-else/if-else.js
@@ -0,0 +1,15 @@
+const age = prompt("Koks jūsų amžius?");
+
+if (age > 30) {
+    console.log("Nebe pauglys")
+} else {
+    console.log("Ganėtinai jaunas")
+}
+
+// Ternary condition (if else alternatyva)
+
+console.log(
+    age  >= 18 ? 'Pilnametis': 'Nesi pilnametis'
+)
+
+console.log(age)
